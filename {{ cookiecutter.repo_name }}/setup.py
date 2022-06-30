781da9bd3c11356fba4201ca4d417277ea93dad8
wassname <git@wassname.org>
2022-06-30 09:00:22 +0800

    unique package names

--- {{ cookiecutter.repo_name }}/setup.py
+++ {{ cookiecutter.repo_name }}/setup.py
@@ -1,10 +1,11 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name='{{ repo_name }}',
     packages=find_packages(),
     version='0.1.0',
     description='{{ cookiecutter.description }}',
+    package_dir={'': 'src'},
     author='{{ cookiecutter.author_name }}',
     license='proprietary',
 )
