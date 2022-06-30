f16f81e221b9ef437ac2f15b0dc1c68d7bc4edd6
wassname <git@wassname.org>
2022-06-30 09:05:20 +0800

    fix

--- {{ cookiecutter.repo_name }}/setup.py
+++ {{ cookiecutter.repo_name }}/setup.py
@@ -5,7 +5,7 @@ setup(
     packages=find_packages(),
     version='0.1.0',
     description='{{ cookiecutter.description }}',
-    package_dir={'': 'src'},
+    package_dir={'{{ cookiecutter.repo_name }}': 'src'},
     author='{{ cookiecutter.author_name }}',
     license='proprietary',
 )
