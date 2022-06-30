58341790322424ded0e575d19045e9fc40122766
wassname <git@wassname.org>
2022-06-30 10:08:25 +0800

    revert

--- {{ cookiecutter.repo_name }}/setup.py
+++ {{ cookiecutter.repo_name }}/setup.py
@@ -1,11 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='{{ cookiecutter.repo_name }}',
+    name='src',
     packages=find_packages(),
     version='0.1.0',
     description='{{ cookiecutter.description }}',
-    package_dir={'{{ cookiecutter.repo_name }}': 'src'},
     author='{{ cookiecutter.author_name }}',
     license='proprietary',
 )
